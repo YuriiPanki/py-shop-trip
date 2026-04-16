c225fa8aaca4b8de29019284dccbea75aba9e584
Yurii <pamkivurij92@gmail.com>
2026-04-16 20:49:46 +0200

    Solution 6

--- app/customer.py
+++ app/customer.py
@@ -1,9 +1,7 @@
-import datetime
-
 from math import sqrt
+from typing import Any
 
 from app.car import Car
-from app.shop import Shop
 
 
 class Customer:
@@ -23,46 +21,36 @@ class Customer:
 
     def count_check(
             self,
-            shop: Shop,
-            printing: bool = False
+            shop: Any,
     ) -> float:
         milk = self.product_cart["milk"] * shop.products["milk"]
         bread = self.product_cart["bread"] * shop.products["bread"]
         butter = self.product_cart["butter"] * shop.products["butter"]
         result = milk + bread + butter
-        print(
-            f"Date: {datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}\n"
-            f"Thanks, {self.name}, for your purchase!\n"
-            f"You have bought:\n"
-            f"{self.product_cart['milk']} milks for {milk} dollars\n"
-            f"{self.product_cart['bread']} breads for {int(bread)} dollars\n"
-            f"{self.product_cart['butter']} butters for {butter} dollars\n"
-            f"Total cost is {result} dollars\n"
-            f"See you again!\n"
-        ) if printing else result
         return result
 
-    def trip(self, shop: Shop, fuel_price: float) -> float:
+    def trip(self, shop: Any, fuel_price: float) -> float:
         return round(
             sqrt(
                 pow(self.location[0] - shop.location[0], 2)
                 + pow(self.location[1] - shop.location[1], 2)
-            ) * self.car.fuel_consumption / 100 * 2 * fuel_price,
+            ) * self.car.fuel_consumption / 100 * 2 * fuel_price
+            + self.count_check(shop),
             2
-        ) + self.count_check(shop, False)
+        )
 
-    def acting(self, shops: list[Shop], fuel_price: float) -> None:
+    def acting(self, shops: list, fuel_price: float) -> None:
         print(f"{self.name} has {self.money} dollars")
-        cost_ = []
+        ct_ = []
         for shop in shops:
-            cost_.append(self.trip(shop, fuel_price))
-            print(f"{self.name}'s trip to the {shop.name} costs {cost_[-1]}")
+            ct_.append(self.trip(shop, fuel_price))
+            print(f"{self.name}'s trip to the {shop.name} costs {ct_[-1]:.2f}")
 
-        min_value = min(cost_)
-        shop_min_cost = shops[cost_.index(min(cost_))]
+        min_value = min(ct_)
+        shop_min_cost = shops[ct_.index(min(ct_))]
         if min_value < self.money:
             print(f"{self.name} rides to {shop_min_cost.name}", end="\n\n")
-            self.count_check(shop_min_cost, True)
+            shop_min_cost.print_check(self)
             print(f"{self.name} rides home")
             print(f"{self.name} now has {self.money - min_value} dollars")
             print()
--- app/shop.py
+++ app/shop.py
@@ -1,3 +1,8 @@
+import datetime
+
+from app.customer import Customer
+
+
 class Shop:
     def __init__(
             self,
@@ -8,3 +13,22 @@ class Shop:
         self.name = name
         self.location = location
         self.products = products
+
+    def print_check(
+            self,
+            cust: Customer,
+    ) -> None:
+        milk = cust.product_cart["milk"] * self.products["milk"]
+        bread = cust.product_cart["bread"] * self.products["bread"]
+        butter = cust.product_cart["butter"] * self.products["butter"]
+        result = milk + bread + butter
+        print(
+            f"Date: {datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}\n"
+            f"Thanks, {cust.name}, for your purchase!\n"
+            f"You have bought:\n"
+            f"{cust.product_cart['milk']} milks for {round(milk)} dollars\n"
+            f"{cust.product_cart['bread']} breads for {round(bread)} dollars\n"
+            f"{cust.product_cart['butter']} butters for {butter} dollars\n"
+            f"Total cost is {result} dollars\n"
+            f"See you again!\n"
+        )
